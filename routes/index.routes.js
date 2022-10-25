91f0d88e8fcdef785d1bfe73ff3ae50724f336a0
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-10-25 12:13:45 +0200

    api routes done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,2 +1,27 @@
 const router = require("express").Router();
 const uploadCloud = require("../cloudinary");
+const Cars = require('../models/Cars.model')
+
+
+router.get("/cars", (req,res) => {
+    console.log("Requesting Car List")
+
+    Cars.find()
+        .then(response => res.status(200).data(response)
+        .catch(err => console.log(err))
+        
+        )
+
+})
+
+
+
+router.get("/bestDeals", (req, res) => {
+    console.log("Requesting Best Deals")
+
+    Cars.find({bestDeals: true})
+        .then(response => res.status(200).data(response)
+        .catch(err => console.log(err))
+        )
+})
+
