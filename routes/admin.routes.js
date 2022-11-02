770a7132558eac0450c0373268fb17c0ec0f012a
Bidz42 <abid97r@gmail.com>
2022-11-02 11:41:02 +0000

    auth context set and protected

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,32 +2,36 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
-const { isAuthenticated } = require("../jwt");
+const Admin = require("../models/Admin.model");
 
 router.post("/login", (req, res) => {
   console.log("Logging In Admin Mode");
 
-  const { email, password } = req.body;
+  const { identifier, password } = req.body;
+  console.log(req.body)
 
-  if (email === "" || password === "") {
+  if (identifier === "" || password === "") {
     res.status(400).json({ message: "Provide an email and password" });
     return;
   }
 
-  User.findOne({ email })
+  Admin.findOne({ identifier })
     .then((loginUser) => {
+      console.log(loginUser)
       if (!loginUser) {
         res.status(400).json({ message: "User not found." });
         return;
       }
       const passedPassword = bcrypt.compareSync(password, loginUser.password);
+      console.log(passedPassword)
       if (passedPassword) {
-        const { _id, name, email } = loginUser;
-        const payload = { _id, name, email };
+        const { _id, identifier } = loginUser;
+        const payload = { _id, identifier };
         const authToken = jwt.sign(payload, process.env.TOKEN_SECRET, {
           algorithm: "HS256",
           expiresIn: "2h",
         });
+        console.log(authToken)
         res.status(200).json({ authToken: authToken });
       } else {
         res.status(401).json({ message: "Cannot authenticate the user" });
@@ -36,7 +40,7 @@ router.post("/login", (req, res) => {
     .catch((err) => res.status(500).json({ message: "Server Error" }));
 });
 
-router.get("/verify", isAuthenticated, (req, res) => {
+router.get("/verify", (req, res) => {
   res.status(200).json(req.payload);
 });
 
