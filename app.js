16c4afdd8333561582c13e41d924abd0e0d44402
Bidz42 <abid97r@gmail.com>
2022-11-05 18:11:10 +0000

    serve js

--- app.js
+++ app.js
@@ -1,26 +1,12 @@
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require("express");
-
+const cors = require("cors");
 const app = express();
 
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
-const PORT = process.env.PORT || 5005;
 
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
-require("dotenv/config");
+require("dotenv").config();
 
-// ℹ️ Connects to the database
 require("./db");
 
-// ℹ️ Needed to accept from requests from 'the outside'. CORS stands for cross origin resource sharing
-// unless the request if from the same domain, by default express wont accept POST requests
-const cors = require("cors");
-
-// Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
-// Services like heroku use something called a proxy and you need to add this to your server
-app.set("trust proxy", 1);
 
 // controls a very specific header to pass headers from the frontend
 app.use(
@@ -32,11 +18,9 @@ app.use(
 
 // To have access to `body` property in the request
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
-// 👇 Start handling routes here
-// Contrary to the views version, all routes are controlled from the routes/index.js
-
+//Routes
 const adminRoutes = require("./routes/admin.routes");
 app.use("/admin", adminRoutes);
 
@@ -49,7 +33,8 @@ app.use("/contact", contactRoutes);
 const testimonialRoutes = require("./routes/testimonial.routes");
 app.use("/testimonial", testimonialRoutes);
 
-module.exports = app;
+// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
