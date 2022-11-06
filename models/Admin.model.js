e08ad25c83194942d8202171707d939528e0b3f5
Muhammed Abidur Rahman <107894012+Bidz42@users.noreply.github.com>
2022-11-06 17:35:15 +0000

    Delete models directory

--- models/Admin.model.js
+++
@@ -1,9 +0,0 @@
-const { Schema, model } = require("mongoose")
-
-const adminSchema  = new Schema({
-    identifier: String,
-    password: String,
-})
-
-const Admin = model("Admin", adminSchema);
-module.exports = Admin;
